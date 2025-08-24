17db577533793bc441a947e2a591193c6aae1b7a
Athrv10 <atharvkamble10@gmail.com>
2025-08-24 12:19:06 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAi Project',
+    version= '0.0.0',
+    author= 'Atharv Kamble',
+    author_email= 'atharvkamble10@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+    
+)
